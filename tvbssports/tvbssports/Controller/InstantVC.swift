9249e06e5e53ad98a73e9afa93e1af48fb3915e3
Oscar <oscarchang@tvbs.com.tw>
2023-11-03 16:28:34 +0800

    feat: 加入tab bar 結構與 splash

---
+++ tvbssports/tvbssports/Controller/InstantVC.swift
@@ -0,0 +1,19 @@
+//
+//  InstantVC.swift
+//  tvbssports
+//
+//  Created by Oscar on 2023/11/3.
+//
+
+import UIKit
+
+class InstantVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
