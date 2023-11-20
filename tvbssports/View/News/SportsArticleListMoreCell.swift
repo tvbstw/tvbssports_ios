6ad707376f2642f3620196542ceedee399434a42
Oscar <oscarchang@tvbs.com.tw>
2023-11-20 16:37:52 +0800

    feat: 新增新聞list Cell

---
+++ tvbssports/View/News/SportsArticleListMoreCell.swift
@@ -0,0 +1,26 @@
+//
+//  SportsArticleListMoreCell.swift
+//  tvbssports
+//
+//  Created by Oscar on 2023/11/20.
+//  Copyright © 2023 Eddie. All rights reserved.
+//
+
+
+import UIKit
+
+class SportsArticleListMoreCell: UITableViewCell {
+
+    @IBOutlet weak var titleLbl: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
